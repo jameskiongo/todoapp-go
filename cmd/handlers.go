f6460b42b9896ad4bc858d5f4f86813ed941f509
jameskiongo <kiongojames2000@gmail.com>
2025-10-28 15:28:00 +0300

    added default routes

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -22,4 +25,27 @@ func home(w http.ResponseWriter, r *http.Request) {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["task"]
+	fmt.Println("Get tasks")
+	fmt.Fprintf(w, "%s", title)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Adding task ...")
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprintf(w, "Updated %s...", id)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprintf(w, "Deleted task %s...", id)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	r := mux.NewRouter()
+	// mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("GET /{$}", home)
+	r.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/add", createTask).Methods("POST")
+	r.HandleFunc("/task/{id}", getTasks).Methods("GET")
+	r.HandleFunc("/task/{id}", updateTask).Methods("PUT")
+	r.HandleFunc("/task/{id}", deleteTask).Methods("DELETE")
 	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":4000", r)
 	log.Fatal(err)
 }

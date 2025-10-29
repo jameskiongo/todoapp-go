70b43e7f025b22d207c836ff06dd7385574068cf
jameskiongo <kiongojames2000@gmail.com>
2025-10-29 14:36:54 +0300

    added and connected to postgresql

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 
 	"github.com/gorilla/mux"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "james"
+	password = "james"
+	dbname   = "todo"
+)
+
 func main() {
 	r := mux.NewRouter()
-	// mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Connecting sql
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, error := sql.Open("postgres", psqlInfo)
+	if error != nil {
+		panic(error)
+	}
+	defer db.Close()
+
+	error = db.Ping()
+	if error != nil {
+		panic(error)
+	} else {
+		fmt.Println("Database connected")
+	}
 	r.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	r.HandleFunc("/", home).Methods("GET")
 	r.HandleFunc("/add", createTask).Methods("POST")
--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 )
